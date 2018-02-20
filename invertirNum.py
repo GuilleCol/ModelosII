e68bf7c6842306e0d12a478a0fc9a79c58ec30d3
ojhernandezcepeda <36193487+ojhernandezcepeda@users.noreply.github.com>
2018-02-20 12:19:39 -0500

    6. Invertir Numero

--- invertirNum.py
+++ invertirNum.py
@@ -9,8 +9,8 @@ def invertir(numero):
     if(numero < 10):
         return numero
     else:
-        return 10*(numero%10) + invertir(int(numero/10))
+        return (10**(numeroDigitos(numero)-1))*(numero%10) + invertir(int(numero/10))
     
 
-print(invertir(123))
+print(invertir(12345678))
 
